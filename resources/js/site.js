cb941218aa6dea0c5927cc6d63680095a8c952e0
Patrick Kerby <patrick@kerbydesign.com>
2022-03-22 00:21:43 -0600

    test IG

--- resources/js/site.js
+++ resources/js/site.js
@@ -18,3 +18,31 @@ $(document).ready(function(){
     adaptiveHeight: false
   });
 });
+
+var request = new XMLHttpRequest();
+			request.open('GET', 'https://graph.facebook.com/v13.0/me/accounts?access_token=IGQVJWNHhmUF9QXzZATOVlHRU9ZAVjVTSHM4V2Q4c0c4ek1vSVZArbVBQeTJYS3EtOUQ1UHVsQ1ZAwb0dQQkNZAeFMwenhNaTU3TUd5cFFtZAHpVWEE3cVhJbXNvdEFYNnkxbHZArMmFBRERJVW1sdjVxTzZAyNlc2Slg2Um93LWR3&count=9', true);
+
+			request.onload = function(container) {
+			  if (request.status >= 200 && request.status < 400) {
+			    // Success!
+			    var data = JSON.parse(request.responseText);
+			    for (var i=0;i < data.data.length;i++) {
+			    	var container = document.getElementById('insta-feed');
+			    	var imgURL = data.data[i].images.standard_resolution.url;
+			    	console.log(imgURL);
+					var div = document.createElement('div');
+			        div.setAttribute('class','instapic');
+			        container.appendChild(div);
+			        var img = document.createElement('img');
+			        img.setAttribute('src',imgURL)
+			        div.appendChild(img);
+			    }
+
+			    console.log(data);
+			  } else {
+			  }
+			};
+			request.onerror = function() {
+			  // There was a connection error of some sort
+			};
+			request.send();
