26d21909e5b3ed1b6d4cfb0df0edd85d6d5577b6
Patrick Kerby <patrick@kerbydesign.com>
2022-03-22 11:34:01 -0600

    test IG

--- resources/js/site.js
+++ resources/js/site.js
@@ -23,3 +23,21 @@ $(document).ready(function(){
     accessToken: 'IGQVJWT1lxMUU3OHl3WUdVRXRqRzN5R0pXYjlrVjJEX25BVkE5OFUwQnJDWEtuWUFNUllMaGw3ZAmtjWjdKZAk9HZAzlJNXFKRENXWTZAGdGJoYUhyNjBvZA0t4N1U2ckg3bjlZAODlkVzNWVUpEN0FZAOUZArVTFBM2xoSUNpbkRN'
   });
   feed.run();
+
+  // create two separate instances of Instafeed
+var bontonFeed = new Instafeed({
+  target: 'bonton',
+  clientId: '3030563962',
+  accessToken: 'IGQVJWT1lxMUU3OHl3WUdVRXRqRzN5R0pXYjlrVjJEX25BVkE5OFUwQnJDWEtuWUFNUllMaGw3ZAmtjWjdKZAk9HZAzlJNXFKRENXWTZAGdGJoYUhyNjBvZA0t4N1U2ckg3bjlZAODlkVzNWVUpEN0FZAOUZArVTFBM2xoSUNpbkRN'
+  // rest of settings
+});
+var rbgFeed = new Instafeed({
+  target: 'riverbendgardens',
+  clientId: '728851544',
+  accessToken: 'IGQVJWT1lxMUU3OHl3WUdVRXRqRzN5R0pXYjlrVjJEX25BVkE5OFUwQnJDWEtuWUFNUllMaGw3ZAmtjWjdKZAk9HZAzlJNXFKRENXWTZAGdGJoYUhyNjBvZA0t4N1U2ckg3bjlZAODlkVzNWVUpEN0FZAOUZArVTFBM2xoSUNpbkRN'
+  // rest of settings
+});
+
+// run both feeds
+bontonFeed.run();
+rbgFeed.run();
